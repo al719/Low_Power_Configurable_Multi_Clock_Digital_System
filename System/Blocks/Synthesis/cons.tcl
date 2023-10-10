dc29464980b71d2df68b58b730a10c609d23042e
al719 <88440404+al719@users.noreply.github.com>
2023-10-10 05:18:09 +0300

    synthesis

---
+++ System/Blocks/Synthesis/cons.tcl
@@ -0,0 +1,174 @@
+
+####################################################################################
+# Constraints
+# ----------------------------------------------------------------------------
+#
+# 0. Design Compiler variables
+#
+# 1. Master Clock Definitions
+#
+# 2. Generated Clock Definitions
+#
+# 3. Clock Uncertainties
+#
+# 4. Clock Latencies 
+#
+# 5. Clock Relationships
+#
+# 6. set input/output delay on ports
+#
+# 7. Driving cells
+#
+# 8. Output load
+
+####################################################################################
+           #########################################################
+                  #### Section 0 : DC Variables ####
+           #########################################################
+#################################################################################### 
+
+# Prevent assign statements in the generated netlist (must be applied before compile command)
+set_fix_multiple_port_nets -all -buffer_constants -feedthroughs
+
+####################################################################################
+           #########################################################
+                  #### Section 1 : Clock Definition ####
+           #########################################################
+#################################################################################### 
+# 1. Master Clock Definitions 
+# 2. Generated Clock Definitions
+# 3. Clock Latencies
+# 4. Clock Uncertainties
+# 4. Clock Transitions
+####################################################################################
+
+set CLK_NAME_REF REF_CLK
+set CLK_NAME_UART UART_CLK
+set CLK_PER_REF 10
+set CLK_PER_UART 271.267361
+set CLK_PER_TX 8680.55556
+#set CLK_SETUP_SKEW_REF 0.2
+set CLK_SETUP_SKEW 0.2
+set CLK_HOLD_SKEW 0.1
+set CLK_LAT 0
+set CLK_RISE 0.05
+set CLK_FALL 0.05
+
+
+create_clock -name $CLK_NAME_REF -period $CLK_PER_REF -waveform "0 [expr $CLK_PER_REF/2]" [get_ports REF_CLK]
+set_clock_uncertainty -setup $CLK_SETUP_SKEW [get_clocks $CLK_NAME_REF]
+set_clock_uncertainty -hold $CLK_HOLD_SKEW  [get_clocks $CLK_NAME_REF]
+set_clock_transition -rise $CLK_RISE  [get_clocks $CLK_NAME_REF]
+set_clock_transition -fall $CLK_FALL  [get_clocks $CLK_NAME_REF]
+set_clock_latency $CLK_LAT [get_clocks $CLK_NAME_REF]
+
+create_generated_clock -master_clock $CLK_NAME_REF -source [get_ports REF_CLK] \
+                       -name "GATED_CLK" [get_port clkGate/GATED_CLK] \
+                       -divide_by 1
+
+set_clock_uncertainty -setup $CLK_SETUP_SKEW [get_clocks GATED_CLK]
+set_clock_uncertainty -hold $CLK_HOLD_SKEW  [get_clocks GATED_CLK]
+set_clock_transition -rise $CLK_RISE  [get_clocks GATED_CLK]
+set_clock_transition -fall $CLK_FALL  [get_clocks GATED_CLK]
+set_clock_latency $CLK_LAT [get_clocks GATED_CLK]
+
+
+
+
+create_clock -name $CLK_NAME_UART -period $CLK_PER_UART -waveform "0 [expr $CLK_PER_UART/2]" [get_ports UART_CLK]
+set_clock_uncertainty -setup $CLK_SETUP_SKEW [get_clocks $CLK_NAME_UART]
+set_clock_uncertainty -hold $CLK_HOLD_SKEW  [get_clocks $CLK_NAME_UART]
+set_clock_transition -rise $CLK_RISE  [get_clocks $CLK_NAME_UART]
+set_clock_transition -fall $CLK_FALL  [get_clocks $CLK_NAME_UART]
+set_clock_latency $CLK_LAT [get_clocks $CLK_NAME_UART]
+
+
+create_generated_clock -master_clock $CLK_NAME_UART -source [get_ports UART_CLK] \
+                       -name "TX_CLK" [get_port u0/o_div_clk] \
+                       -divide_by 32
+
+set_clock_uncertainty -setup $CLK_SETUP_SKEW [get_clocks TX_CLK]
+set_clock_uncertainty -hold $CLK_HOLD_SKEW  [get_clocks TX_CLK]
+set_clock_transition -rise $CLK_RISE  [get_clocks TX_CLK]
+set_clock_transition -fall $CLK_FALL  [get_clocks TX_CLK]
+set_clock_latency $CLK_LAT [get_clocks TX_CLK]
+
+
+create_generated_clock -master_clock $CLK_NAME_UART -source [get_ports UART_CLK] \
+                       -name "RX_CLK" [get_port u1/o_div_clk] \
+                       -divide_by 1
+
+set_clock_uncertainty -setup $CLK_SETUP_SKEW [get_clocks RX_CLK]
+set_clock_uncertainty -hold $CLK_HOLD_SKEW  [get_clocks RX_CLK]
+set_clock_transition -rise $CLK_RISE  [get_clocks RX_CLK]
+set_clock_transition -fall $CLK_FALL  [get_clocks RX_CLK]
+set_clock_latency $CLK_LAT [get_clocks RX_CLK]
+
+
+set_dont_touch_network {REF_CLK UART_CLK RST}
+
+####################################################################################
+           #########################################################
+             #### Section 2 : Clocks Relationship ####
+           #########################################################
+####################################################################################
+
+set_clock_groups -asynchronous -group [get_clocks "REF_CLK   GATED_CLK"]  \
+                               -group [get_clocks "UART_CLK TX_CLK RX_CLK"] 
+#set_false_path -from [get_clocks "RX_CLK" ] -to [get_clocks "REF_CLK" ]
+#set_false_path -from [get_clocks "REF_CLK" ] -to [get_clocks "TX_CLK" ]
+#set_false_path -from [get_clocks "REF_CLK" ] -to [get_clocks "UART_CLK" ]
+#set_false_path -from [get_clocks "REF_CLK" ] -to [get_clocks "RX_CLK" ]
+####################################################################################
+           #########################################################
+             #### Section 3 : set input/output delay on ports ####
+           #########################################################
+####################################################################################
+set in_delay  [expr 0.2*$CLK_PER_UART]
+set out_delay [expr 0.2*$CLK_PER_TX]
+set out_delay_rx [expr 0.2*$CLK_PER_UART]
+#Constrain Input Paths
+set_input_delay $in_delay -clock $CLK_NAME_UART [get_port RX_IN]
+
+
+#Constrain Output Paths
+set_output_delay $out_delay -clock "TX_CLK" [get_port TX_OUT]
+set_output_delay $out_delay_rx -clock "RX_CLK" [get_port parity_error]
+set_output_delay $out_delay_rx -clock "RX_CLK" [get_port frame_error]
+####################################################################################
+           #########################################################
+                  #### Section 4 : Driving cells ####
+           #########################################################
+####################################################################################
+set_driving_cell -library scmetro_tsmc_cl013g_rvt_ss_1p08v_125c -lib_cell BUFX2M -pin Y [get_port RX_IN]
+
+####################################################################################
+           #########################################################
+                  #### Section 5 : Output load ####
+           #########################################################
+####################################################################################
+set_load 0.5  [get_port TX_OUT]
+set_load 0.5  [get_port parity_error]
+set_load 0.5  [get_port frame_error]
+####################################################################################
+           #########################################################
+                 #### Section 6 : Operating Condition ####
+           #########################################################
+####################################################################################
+
+# Define the Worst Library for Max(#setup) analysis
+# Define the Best Library for Min(hold) analysis
+
+set_operating_conditions -min_library "scmetro_tsmc_cl013g_rvt_ff_1p32v_m40c" -min "scmetro_tsmc_cl013g_rvt_ff_1p32v_m40c" -max_library "scmetro_tsmc_cl013g_rvt_ss_1p08v_125c" -max "scmetro_tsmc_cl013g_rvt_ss_1p08v_125c"
+
+####################################################################################
+           #########################################################
+                  #### Section 7 : wireload Model ####
+           #########################################################
+####################################################################################
+
+#set_wire_load_model -name tsmc13_wl10 -library scmetro_tsmc_cl013g_rvt_ss_1p08v_125c
+
+####################################################################################
+
+
---
+++ System/Blocks/Synthesis/syn_script.tcl
@@ -0,0 +1,174 @@
+
+########################### Define Top Module ############################
+                                                   
+set top_module SYS_TOP
+
+##################### Define Working Library Directory ######################
+                                                   
+define_design_lib work -path ./work
+
+############################# Formality Setup File ##########################
+                                                   
+set_svf $top_module.svf
+
+################## Design Compiler Library Files #setup ######################
+
+puts "###########################################"
+puts "#      #setting Design Libraries          #"
+puts "###########################################"
+
+#Add the path of the libraries and RTL files to the search_path variable
+
+set PROJECT_PATH /home/IC/System/
+set LIB_PATH     /home/IC/tsmc_fb_cl013g_sc/aci/sc-m
+
+lappend search_path $LIB_PATH/synopsys
+lappend search_path $PROJECT_PATH/Blocks/ALU
+lappend search_path $PROJECT_PATH/Blocks/ASYNC_FIFO
+lappend search_path $PROJECT_PATH/Blocks/Clock_Divider
+lappend search_path $PROJECT_PATH/Blocks/Clock_Gating
+lappend search_path $PROJECT_PATH/Blocks/DATA_SYNC
+lappend search_path $PROJECT_PATH/Blocks/RegFile
+lappend search_path $PROJECT_PATH/Blocks/PULSE_GEN
+lappend search_path $PROJECT_PATH/Blocks/RST_SYNC
+lappend search_path $PROJECT_PATH/Blocks/FSM
+lappend search_path $PROJECT_PATH/Blocks/CLKDIV_MUX
+lappend search_path $PROJECT_PATH/Blocks/UART/UART_RX
+lappend search_path $PROJECT_PATH/Blocks/UART/UART_TX
+lappend search_path $PROJECT_PATH/Blocks/Top
+
+set SSLIB "scmetro_tsmc_cl013g_rvt_ss_1p08v_125c.db"
+set TTLIB "scmetro_tsmc_cl013g_rvt_tt_1p2v_25c.db"
+set FFLIB "scmetro_tsmc_cl013g_rvt_ff_1p32v_m40c.db"
+
+## Standard Cell libraries 
+set target_library [list $SSLIB $TTLIB $FFLIB]
+
+## Standard Cell & Hard Macros libraries 
+set link_library [list * $SSLIB $TTLIB $FFLIB]  
+
+######################## Reading RTL Files #################################
+
+puts "###########################################"
+puts "#             Reading RTL Files           #"
+puts "###########################################"
+
+set file_format verilog
+
+#ALU
+analyze -format $file_format ALU.v
+#FIFO
+analyze -format $file_format FIFO.v
+analyze -format $file_format FIFO_Empty.v
+analyze -format $file_format FIFO_Full.v
+analyze -format $file_format Ram.v
+analyze -format $file_format Synchronizer.v
+#CLK_DIVIDER MUX
+analyze -format $file_format custom_mux.v
+#CLK_DIVIDER
+analyze -format $file_format ClkDiv.v
+#CLK_GATING
+analyze -format $file_format CLK_GATE.v
+#DATA_SYNC
+analyze -format $file_format DATA_SYNC.v
+#REGISTER_FILE
+analyze -format $file_format RegisterFile.v
+#PULSE_GENERATOR
+analyze -format $file_format PULSE_GEN.v
+analyze -format $file_format PULSE_NEG_GEN.v
+analyze -format $file_format PULSE_SAMEZERO_GEN.v
+#RST_SYNC
+analyze -format $file_format RST_SYNC.v
+#SYS_CONTROLLER
+analyze -format $file_format SYS_FSM.v
+#UART_RX
+analyze -format $file_format data_sampling.v
+analyze -format $file_format deserializer.v
+analyze -format $file_format edge_bit_counter.v
+analyze -format $file_format parity_chk.v
+analyze -format $file_format stp_chk.v
+analyze -format $file_format strt_chk.v
+analyze -format $file_format UART_RX.v
+analyze -format $file_format FSM_RX.v
+#UART_TX
+#analyze -format $file_format mux.v
+analyze -format $file_format parityCalc.v
+analyze -format $file_format serializer.v
+analyze -format $file_format UART_TX.v
+analyze -format $file_format FSM.v
+#UART_TOP
+#analyze -format $file_format UART.v
+#SYS_TOP
+analyze -format $file_format SYS_TOP.v
+
+elaborate -lib WORK SYS_TOP
+
+###################### Defining toplevel ###################################
+
+current_design $top_module
+
+#################### Liniking All The Design Parts #########################
+puts "###############################################"
+puts "######## Liniking All The Design Parts ########"
+puts "###############################################"
+
+link 
+
+#################### Liniking All The Design Parts #########################
+puts "###############################################"
+puts "######## checking design consistency ##########"
+puts "###############################################"
+
+check_design >> reports/check_design.rpt
+
+#################### Define Design Constraints #########################
+puts "###############################################"
+puts "############ Design Constraints #### ##########"
+puts "###############################################"
+
+source -echo ./cons.tcl
+
+###################### Mapping and optimization ########################
+puts "###############################################"
+puts "########## Mapping & Optimization #############"
+puts "###############################################"
+
+compile -map_effort high
+#compile_ultra -no_auto_ungroup
+##################### Close Formality Setup file ###########################
+
+set_svf -off
+
+#############################################################################
+# Write out files
+#############################################################################
+
+write_file -format verilog -hierarchy -output netlists/$top_module.ddc
+write_file -format verilog -hierarchy -output netlists/$top_module.v
+write_sdf  sdf/$top_module.sdf
+write_sdc  -nosplit sdc/$top_module.sdc
+
+####################### reporting ##########################################
+
+report_area -hierarchy > reports/area.rpt
+report_power -hierarchy > reports/power.rpt
+report_timing -delay_type min -max_paths 20 > reports/hold.rpt
+report_timing -delay_type max -max_paths 20 > reports/setup.rpt
+report_clock -attributes > reports/clocks.rpt
+report_constraint -all_violators -nosplit > reports/constraints.rpt
+
+############################################################################
+# DFT Preparation Section
+############################################################################
+
+set flops_per_chain 100
+
+set num_flops [sizeof_collection [all_registers -edge_triggered]]
+
+set num_chains [expr $num_flops / $flops_per_chain + 1 ]
+
+################# starting graphical user interface #######################
+
+#gui_start
+
+#exit
